a43675ae58776da012056e06adfbcb67a848ec2b
Narendranath Gogineni <narenrockstar1@gmail.com>
2024-09-29 13:00:50 +0530

    change id key

--- chintamani/utils.py
+++ chintamani/utils.py
@@ -1,6 +1,6 @@
 import uuid
 
 def create_node(data, label_key):
-    data['uuid'] = str(uuid.uuid4())
+    data['id'] = str(uuid.uuid4())
     data['title'] = data[label_key]
     return {key: value for key, value in data.items() if value is not None}
