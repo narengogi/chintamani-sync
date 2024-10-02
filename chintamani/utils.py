fc98e1036930b44e356ff1d7c4681c768ff22da5
Narendranath Gogineni <narenrockstar1@gmail.com>
2024-10-02 21:08:45 +0530

    move id creation to chintamani

--- chintamani/utils.py
+++ chintamani/utils.py
@@ -1,7 +1,6 @@
 import uuid
 
 def create_chintamani_node(data, label_key):
-    data['id'] = str(uuid.uuid4())
     data['title'] = data[label_key]
     del data[label_key]
     return {key: value for key, value in data.items() if value is not None}
